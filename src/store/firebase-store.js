001de76e97e7f448885d6293fabf0711c4dcc851
jia-wei-00 <wwwbingo6@gmail.com>
2023-04-06 00:43:18 +0800

    add user login and google auth

--- src/store/firebase-store.js
+++ src/store/firebase-store.js
@@ -1,41 +1,51 @@
 import { makeObservable, action, observable } from "mobx";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import db, { auth, fieldValue } from "../firebase";
+import db, { auth, provider } from "../firebase";
+
 // import { sendPasswordResetEmail } from "firebase/auth";
 
 export class firebaseStoreImplementation {
-  user = "";
+  user = null;
+  username = null;
 
   constructor() {
     makeObservable(this, {
       user: observable,
-      getUserAuth: action.bound,
+      setUser: action.bound,
+      signInAPI: action.bound,
+      signOut: action.bound,
     });
-  }
 
-  getUserAuth() {
     auth.onAuthStateChanged((user) => {
       if (user) {
-        this.user = user
+        this.setUser(user);
+      } else {
+        this.setUser(null);
       }
     });
-
   }
 
-  signInAPI(data) {
+  setUser(user) {
+    this.user = user;
+  }
+
+  setUsername(username) {
+    this.username = username;
+  }
+
+  signInAPI(email, password) {
     const id = toast.loading("Please wait...");
     auth
-      .signInWithEmailAndPassword(data.username, data.password)
-      .then((auth) => {
+      .signInWithEmailAndPassword(email, password)
+      .then((user) => {
         toast.update(id, {
-          render: "Welcome " + auth.user.email,
+          render: "Welcome " + user.user.email,
           type: "success",
           isLoading: false,
           autoClose: 5000,
         });
-
-        this.user = auth.user;
+        this.setUser(user.user);
       })
       .catch((error) => {
         toast.update(id, {
@@ -47,6 +57,53 @@ export class firebaseStoreImplementation {
       });
   }
 
+  googleSignIn() {
+    const id = toast.loading("Please wait...");
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        // User signed in successfully, handle the result object
+        toast.update(id, {
+          render: "Welcome " + result.user.displayName,
+          type: "success",
+          isLoading: false,
+          autoClose: 5000,
+        });
+        this.setUser(result.user.email);
+        this.setUsername(result.user.displayName);
+      })
+      .catch((error) => {
+        toast.update(id, {
+          render: error.message,
+          type: "error",
+          isLoading: false,
+          autoClose: 5000,
+        });
+      });
+  }
+
+  signOut() {
+    const id = toast.loading("Please wait...");
+    auth
+      .signOut()
+      .then(() => {
+        toast.update(id, {
+          render: "Successfully Logout",
+          type: "success",
+          isLoading: false,
+          autoClose: 5000,
+        });
+        this.setUser(null);
+      })
+      .catch((error) => {
+        toast.update(id, {
+          render: error.message,
+          type: "error",
+          isLoading: false,
+          autoClose: 5000,
+        });
+      });
+  }
 }
 
 const firebaseStore = new firebaseStoreImplementation();
