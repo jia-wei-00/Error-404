6a9125602d03796489da23c9ad5d0b26113c9a66
FriendlyBen <bentleyteh2001@gmail.com>
2023-04-06 11:52:12 +0800

    update 1st commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Modal } from "./details-modal";
 export { default as Nav } from "./nav";
 export { default as Wrapper } from "./wrapper";
+export { default as StickyHeadTable } from "./table";
+
